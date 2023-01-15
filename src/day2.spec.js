10c6bdf3b19f029906ab2bbe16c6d4d6ba398b94
gabor.schneider <gabor.schneider@4ig.hu>
2023-01-15 17:36:35 +0100

    day2 seed

---
+++ src/day2.spec.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+
+const day2 = require('./day2')
+
+describe('day2', () => {
+  it('should work with seed data', () => {
+    const data = `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`
+    const result = day2(data)
+    expect(result).to.eql(45000)
+  })
+  // it('should with with puzzle input', function () {
+  //   const data = fs.readFileSync(__dirname+'/day1.seed', 'utf8')
+  //   const result = day1(data)
+  //   console.log('result', typeof result, JSON.stringify(result))
+  //   // 69206
+  // })
+})
