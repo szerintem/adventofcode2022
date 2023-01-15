9cce708dc73861de9d618a90c411144aff9caf2d
gabor.schneider <gabor.schneider@4ig.hu>
2023-01-15 17:28:45 +0100

    day1 seed

--- src/day1.js
+++ src/day1.js
@@ -1,6 +1,24 @@
 
-const day1 = function() {
-  return 'Hello World'
+const day1 = function(data) {
+  const rows = data.split("\n")
+
+  let current = 0
+  const blocks = []
+
+  for(const rowNumber in rows) {
+    const rowContent = rows[rowNumber]
+
+    if(rowContent === '') {
+      blocks.push(current)
+      current = 0
+      continue
+    }
+
+    current += parseInt(rowContent)
+  }
+  blocks.push(current)
+
+  return Math.max(...Object.values(blocks))
 }
 
 module.exports = day1
--- src/day1.spec.js
+++ src/day1.spec.js
@@ -2,7 +2,21 @@
 const day1 = require('./day1')
 
 describe('day1', () => {
-  it('should be a function', () => {
-    expect(day1).to.instanceof(Function)
+  it('should work with seed data', () => {
+    const data = `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`
+    expect(day1(data)).to.eql(24000)
   })
 })
